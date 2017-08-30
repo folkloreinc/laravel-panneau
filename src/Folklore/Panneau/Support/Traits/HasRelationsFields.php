7bb917a124579ff2c73828712024ec22d474ac62
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-08-30 15:00:23 -0400

    Fix with relations

--- src/Folklore/Panneau/Support/Traits/HasRelationsFields.php
+++ src/Folklore/Panneau/Support/Traits/HasRelationsFields.php
@@ -167,8 +167,10 @@ trait HasRelationsFields
         if (method_exists($this, $method)) {
             return $this->{$method}($relation, $id);
         }
+        // TODO: FIX THIS MOTHERFUCKING SITUATION WHERE A RELATION IS NOT LOADED
+        // (possible cause is having an appends the same name as a relations)
         if (!$this->relationLoaded($relation)) {
-            $this->load($relation);
+            return null;
         }
         return $this->getRelation($relation)->first(function ($item) use ($relation, $id) {
             return $this->getRelationIdFromItem($relation, $item) === (string)$id;
