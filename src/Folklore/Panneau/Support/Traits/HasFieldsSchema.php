83185c11248a4c7964bc45ae81f6639b98206aec
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2017-09-14 11:56:47 -0400

    Fix with toArray

--- src/Folklore/Panneau/Support/Traits/HasFieldsSchema.php
+++ src/Folklore/Panneau/Support/Traits/HasFieldsSchema.php
@@ -420,9 +420,9 @@ trait HasFieldsSchema
         $appendsAttributes = [];
         foreach ($this->getFieldsAppends() as $key => $path) {
             if (is_numeric($key)) {
-                $appendsAttributes[$path] = array_get($fieldsAttributes, $path);
+                $appendsAttributes[$path] = $this->fieldsAttributes()->get($path);
             } else {
-                $appendsAttributes[$key] = array_get($fieldsAttributes, $path);
+                $appendsAttributes[$key] = $this->fieldsAttributes()->get($path);
             }
         }
 
