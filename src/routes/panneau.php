719b43f63d553fd3ed608c004d3eea2313697957
Hugo St-Onge <hso@atelierfolklore.ca>
2018-01-26 11:57:43 -0500

    Fix routes for resources with custom controller

--- src/routes/panneau.php
+++ src/routes/panneau.php
@@ -3,33 +3,32 @@
 $prefix = config('panneau.route.prefix');
 $namespace = config('panneau.route.namespace');
 $middleware = config('panneau.route.middleware');
-$resources = config('panneau.resources');
-$paths = config('panneau.route.paths');
+
+$resources = app('panneau')->getDefinition()->getResources();
 
 $router->group([
     'prefix' => $prefix,
     'namespace' => $namespace,
     'middleware' => $middleware,
-], function ($router) use ($resources, $paths) {
+], function ($router) use ($resources) {
     $resourcesMatchIds = [];
-    foreach ($resources as $resource => $definition) {
-        $customController = array_get($definition, 'controller');
+    foreach ($resources as $resource) {
+        $customController = $resource->getController();
         if (is_null($customController)) {
             // Add resource to catch-all list
-            $resourcesMatchIds[] = $resource;
+            $resourcesMatchIds[] = $resource->getId();
         } else {
             // Create custom routes set
-            $router->panneauResource($resource, [
+            $router->panneauResource($resource->getId(), [
                 'controller' => $customController,
             ]);
         }
     }
 
     // Create catch-all route
-    $resourcesMatchRegex = implode($resourcesMatchIds, '|');
-    $router->panneauResource('*', [
-        'whereResource' => $resourcesMatchRegex,
-    ]);
+    $router->panneauResource('*', !empty($resourcesMatchIds) ? [
+        'whereResource' => implode($resourcesMatchIds, '|'),
+    ] : null);
 
     // Add the layout routes
     // @TODO move to registrar ?
