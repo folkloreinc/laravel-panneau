65e6334537de38fa36b963d0c1cfa90c878a5c57
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-02-22 11:50:15 -0500

    Translations of auth

---
+++ src/resources/lang/en/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'login_title' => 'Login',
+    'email_label' => 'E-Mail Address',
+    'password_label' => 'Password',
+    'password_confirm_label' => 'Confirm password',
+    'remember_me' => 'Remember me',
+    'login_btn' => 'Login',
+    'forgot_link' => 'Forgot Your Password?',
+    'reset_title' => 'Reset Password',
+    'send_reset_btn' => 'Send Password Reset Link',
+    'reset_btn' => 'Reset Password'
+];
