1b05cc000b8fa117711a341669b58dd892c9d352
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-02-26 15:32:31 -0500

    Removed laravel 5.2

--- src/Folklore/Panneau/helpers.php
+++ src/Folklore/Panneau/helpers.php
@@ -11,17 +11,3 @@ if (!function_exists('panneau')) {
         return app('panneau');
     }
 }
-
-// Missing method in Laravel 5.2
-if (! function_exists('resolve')) {
-    /**
-     * Resolve a service from the container.
-     *
-     * @param  string  $name
-     * @return mixed
-     */
-    function resolve($name)
-    {
-        return app($name);
-    }
-}
