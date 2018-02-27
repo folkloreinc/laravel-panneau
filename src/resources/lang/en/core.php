6cceee98b2cd6542783f772af062d6501350f937
Hugo St-Onge <hso@atelierfolklore.ca>
2018-02-27 15:12:29 -0500

    Update panneau@0.3.67

--- src/resources/lang/en/core.php
+++ src/resources/lang/en/core.php
@@ -4,6 +4,7 @@
 return [
     'buttons.resources.create' => 'Create',
     'buttons.resources.edit' => 'Save',
+    'buttons.resources.add' => 'Add',
     'navbar.resources.index' => 'View all {resourceLabel}',
     'navbar.resources.create' => 'Add a new {resourceLabel}',
 ];
--- src/resources/lang/fr/core.php
+++ src/resources/lang/fr/core.php
@@ -4,6 +4,7 @@
 return [
     'buttons.resources.create' => 'Créer',
     'buttons.resources.edit' => 'Sauvegarder',
+    'buttons.resources.add' => 'Ajouter',
     'navbar.resources.index' => 'Voir tout les {resourceLabel}',
     'navbar.resources.create' => 'Ajouter un nouveau {resourceLabel}',
 ];
