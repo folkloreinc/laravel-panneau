477045ca8e88e30510d33ef24d1726aa8db52634
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-03-10 17:12:09 -0500

    Updated vendor

--- src/resources/lang/en/fields.php
+++ src/resources/lang/en/fields.php
@@ -1,8 +1,14 @@
+
 <?php
 
 return [
-    'slug_label' => 'Slug',
-    'title_label' => 'Title',
-    'parent_label' => 'Parent page',
-    'blocks_label' => 'Blocks'
+    'media.dimension_label' => 'Dimension',
+    'media.duration_label' => 'Duration',
+    'media.pages_label' => 'Pages',
+    'media.size_label' => 'Size',
+    'media.upload_audio_label' => 'Select an audio file...',
+    'media.upload_document_label' => 'Select a document file...',
+    'media.upload_media_label' => 'Select a media file...',
+    'media.upload_picture_label' => 'Select a picture file...',
+    'media.upload_video_label' => 'Select a video file...',
 ];
--- src/resources/lang/fr/fields.php
+++ src/resources/lang/fr/fields.php
@@ -1,8 +1,14 @@
+
 <?php
 
 return [
-    'slug_label' => 'Slug',
-    'title_label' => 'Titre',
-    'parent_label' => 'Page parent',
-    'blocks_label' => 'Blocs'
+    'media.dimension_label' => 'Dimension',
+    'media.duration_label' => 'Durée',
+    'media.pages_label' => 'Pages',
+    'media.size_label' => 'Poids',
+    'media.upload_audio_label' => 'Sélectionnez un fichier audio...',
+    'media.upload_document_label' => 'Sélectionnez un document...',
+    'media.upload_media_label' => 'Sélectionnez un fichier média...',
+    'media.upload_picture_label' => 'Sélectionnez une image...',
+    'media.upload_video_label' => 'Sélectionnez un fichier vidéo...',
 ];
