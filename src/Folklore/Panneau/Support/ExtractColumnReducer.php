2bc28d620b6c35812c829330e33562bbdca340bf
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-03-10 20:35:59 -0500

    Refactor

--- src/Folklore/Panneau/Support/ExtractColumnReducer.php
+++ src/Folklore/Panneau/Support/ExtractColumnReducer.php
@@ -10,14 +10,15 @@ use App\Schemas\Pages\PageSchema;
 
 abstract class ExtractColumnReducer extends Reducer
 {
-    abstract public function getExtractedPaths();
+    abstract protected function getExtractedColumns();
 
     public function set(HasJsonSchema $model, Node $node, $value)
     {
-        $extractedPaths = $this->getExtractedPaths();
-        if (array_key_exists($node->path, $extractedPaths)) {
-            $columnName = $extractedPaths[$node->path];
-            $model->{$columnName} = Utils::getPath($value, $node->path);
+        $extractedColumns = $this->getExtractedColumns();
+        foreach ($extractedColumns as $column => $path) {
+            if ($node->path === $path) {
+                $model->{$column} = Utils::getPath($value, $node->path);
+            }
         }
         return $value;
     }
