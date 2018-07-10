03851b358492cf8f4c27bbf690f02d294bf350ae
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-07-10 13:34:15 -0400

    Removed app boilerplate from the package

--- src/Folklore/Panneau/PanneauServiceProvider.php
+++ src/Folklore/Panneau/PanneauServiceProvider.php
@@ -41,7 +41,6 @@ class PanneauServiceProvider extends ServiceProvider
     public function bootPublishes()
     {
         // Paths
-        $appPath = __DIR__ . '/../../app/';
         $configPath = __DIR__ . '/../../config/config.php';
         $migrationsPath = __DIR__ . '/../../migrations/';
         $viewsPath = __DIR__ . '/../../resources/views/';
@@ -66,10 +65,6 @@ class PanneauServiceProvider extends ServiceProvider
                 $migrationsPath => base_path('database/migrations')
             ], 'migrations');
         }
-
-        $this->publishes([
-            $appPath => base_path('app'),
-        ], 'app');
 
         // Publish
         $this->publishes([
