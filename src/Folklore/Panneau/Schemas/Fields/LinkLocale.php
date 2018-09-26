e23302153ad1cdc11a336bfcb24b0dc3dc399c7f
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-09-26 17:38:25 -0400

    Fix with link locales fields

--- src/Folklore/Panneau/Schemas/Fields/LinkLocale.php
+++ src/Folklore/Panneau/Schemas/Fields/LinkLocale.php
@@ -6,11 +6,23 @@ use Folklore\Panneau\Support\Field;
 
 class LinkLocale extends Field
 {
+    protected function getLocales()
+    {
+        return app('panneau')->locales();
+    }
+
     protected function properties()
     {
         return [
             'url' => field('url_locale'),
             'label' => field('text_locale'),
+        ];
+    }
+
+    protected function attributes()
+    {
+        return [
+            'locales' => $this->getLocales(),
         ];
     }
 }
--- src/Folklore/Panneau/Schemas/Fields/LinksLocale.php
+++ src/Folklore/Panneau/Schemas/Fields/LinksLocale.php
@@ -6,6 +6,11 @@ use Folklore\Panneau\Support\Field;
 
 class LinksLocale extends Field
 {
+    protected function getLocales()
+    {
+        return app('panneau')->locales();
+    }
+
     protected function type()
     {
         return 'array';
@@ -14,5 +19,12 @@ class LinksLocale extends Field
     protected function items()
     {
         return field('link_locale');
+    }
+
+    protected function attributes()
+    {
+        return [
+            'locales' => $this->getLocales(),
+        ];
     }
 }
