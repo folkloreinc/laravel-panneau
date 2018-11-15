ea9079c3a30baf7414cc883f448706b249017cd7
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-11-15 14:45:11 -0500

    Better postcss configuration

--- src/resources/assets/postcss.config.js
+++ src/resources/assets/postcss.config.js
@@ -1,15 +1,14 @@
+/* eslint-disable no-console, global-require */
+/* eslint-disable import/no-extraneous-dependencies, import/no-dynamic-require, import/order */
 module.exports = ({ options }) => ({
-    map: {
-        inline: false,
-    },
-    plugins: {
-        autoprefixer: {},
-        cssnano: {
-            preset: 'default',
-            zindex: false,
-            discardUnused: {
-                fontFace: false,
+    ident: 'postcss',
+    plugins: [
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
+            autoprefixer: {
+                flexbox: 'no-2009',
             },
-        },
-    },
+            stage: 3,
+        }),
+    ],
 });
