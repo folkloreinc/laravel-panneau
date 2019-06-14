ded9fa45d61285f11ff5b0fb36779f64c4d7845b
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2019-06-14 15:46:31 -0400

    Added redirect to panneau auth page

--- src/Folklore/Panneau/Http/Middlewares/Authenticate.php
+++ src/Folklore/Panneau/Http/Middlewares/Authenticate.php
@@ -6,5 +6,14 @@ use Illuminate\Auth\Middleware\Authenticate as BaseAuthenticate;
 
 class Authenticate extends BaseAuthenticate
 {
-
+    /**
+     * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    protected function redirectTo($request)
+    {
+        return route('panneau.auth.login');
+    }
 }
