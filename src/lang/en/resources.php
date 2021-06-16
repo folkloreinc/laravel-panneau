595be47ca22c4ddd4adb1508180de1b00f0e5eec
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2021-06-16 11:53:59 -0400

    Added missing translations

--- src/lang/en/resources.php
+++ src/lang/en/resources.php
@@ -2,7 +2,11 @@
 
 return [
     'create' => 'Create {a_singular}',
+    'create_btn' => 'Create {a_singular}',
     'edit' => 'Edit {a_singular}',
+    'delete' => 'Delete {a_singular}',
+    'view' => 'View {a_singular}',
+    'confirm_delete' => 'Are you sure you want to delete {the_singular} #{id}?',
     'created' => '{The_singular} has been created',
     'deleted' => '{The_singular} has been deleted',
     'loading' => 'Loading {plural}...',
--- src/lang/fr/resources.php
+++ src/lang/fr/resources.php
@@ -2,7 +2,11 @@
 
 return [
     'create' => 'Créer {a_singular}',
+    'create_btn' => 'Créer {a_singular}',
     'edit' => 'Modifier {a_singular}',
+    'delete' => 'Supprimer {a_singular}',
+    'view' => 'Afficher {a_singular}',
+    'confirm_delete' => 'Êtes-vous certain de vouloir supprimer {the_singular} #{id}?',
     'created' => '{The_singular} a été créé',
     'deleted' => '{The_singular} a été supprimé',
     'loading' => 'Chargement des {plural}...',
