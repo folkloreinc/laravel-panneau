2cf5375e32e3a6e38a67f265ce50009bb7817278
Nicolas Roy B. <phatshambler@gmail.com>
2025-02-18 15:51:26 -0500

    Color field options

--- src/Panneau/Fields/Color.php
+++ src/Panneau/Fields/Color.php
@@ -6,6 +6,10 @@ use Panneau\Support\Field;
 
 class Color extends Field
 {
+    protected $withAlpha = false;
+
+    protected $isNative = false;
+
     public function type(): string
     {
         return 'string';
@@ -14,5 +18,25 @@ class Color extends Field
     public function component(): string
     {
         return 'color';
+    }
+
+    public function attributes(): ?array
+    {
+        return array_merge(parent::attributes(), [
+            'withAlpha' => $this->withAlpha,
+            'native' => $this->isNative,
+        ]);
+    }
+
+    public function withAlpha()
+    {
+        $this->withAlpha = true;
+        return $this;
+    }
+
+    public function isNative()
+    {
+        $this->isNative = true;
+        return $this;
     }
 }
